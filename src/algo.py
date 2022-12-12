72ab4c6e654ae58d9675a11ce61c5c548b681b8d
Claudio Gauna <calasius@gmail.com>
2022-12-12 01:19:47 -0300

    doc: add function docstring

--- src/algo.py
+++ src/algo.py
@@ -1,2 +1,11 @@
 def sum(num1: int, num2: int) -> int:
+    """Sum two numbers
+
+    Args:
+        num1 (int): num1
+        num2 (int): mun2
+
+    Returns:
+        int: sum result
+    """
     return num1 + num2
